f41baa55aba97194809182edd45b376b45e28fb9
Shiniga-OP <shinigaop@gmail.com>
2026-02-24 12:18:52 -0300

    UI.java refeita com Micro

--- gdx-jogo/src/com/minimine/ui/InterUtil.java
+++ gdx-jogo/src/com/minimine/ui/InterUtil.java
@@ -1,45 +1,19 @@
 package com.minimine.ui;
 
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.Gdx;
 import com.minimine.Inicio;
-import java.util.Map;
-import java.util.HashMap;
 
 public class InterUtil {
-	public SpriteBatch sb;
-	public BitmapFont fonte;
-	public Map<CharSequence, Objeto> objetos = new HashMap<>();
-
-	public InterUtil(int tamTexto) {
-		sb = new SpriteBatch();
-		fonte = carregarFonte("ui/fontes/pixel.ttf", tamTexto);
+	public static BitmapFont carregarFonte(String caminho) {
+		FreeTypeFontGenerator gerador = new FreeTypeFontGenerator(caminho.equals("ui/fontes/pixel.ttf") ? Gdx.files.internal(caminho) : Gdx.files.absolute(Inicio.externo+"/MiniMine/mods/"+caminho));
+		FreeTypeFontGenerator.FreeTypeFontParameter args = new FreeTypeFontGenerator.FreeTypeFontParameter();
+		args.characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_?!π√×|§°←↑↓→€¥¢£+-/×%^*#$&|=,.:;!?(){}[]<>@\"'~áãâçéêèēóõôò";
+		BitmapFont fonte = gerador.generateFont(args);
+		gerador.dispose();
+		return fonte;
 	}
-
-	public void att(float delta) {
-		for(Objeto b : objetos.values()) {
-			if(b != null) b.porFrame(delta, sb, fonte);
-		}
-	}
-
-	public void ajustar(int v, int h) {
-		for(Objeto b : objetos.values()) {
-			if(b != null) b.aoAjustar(v, h);
-		}
-	}
-
-	public void liberar() {
-		sb.dispose();
-		fonte.dispose();
-		for(Objeto b : objetos.values()) {
-			b.aoFim();
-		}
-		objetos.clear();
-	}
-
 	public static BitmapFont carregarFonte(String caminho, int tamanho) {
 		FreeTypeFontGenerator gerador = new FreeTypeFontGenerator(caminho.equals("ui/fontes/pixel.ttf") ? Gdx.files.internal(caminho) : Gdx.files.absolute(Inicio.externo+"/MiniMine/mods/"+caminho));
 		FreeTypeFontGenerator.FreeTypeFontParameter args = new FreeTypeFontGenerator.FreeTypeFontParameter();
@@ -48,19 +22,5 @@ public class InterUtil {
 		BitmapFont fonte = gerador.generateFont(args);
 		gerador.dispose();
 		return fonte;
-	}
-
-	public static class Objeto {
-		public String nome;
-
-		public Objeto(String nome) {
-			this.nome = nome;
-		}
-		public void porFrame(float delta, SpriteBatch sb, BitmapFont fonte) {}
-		public void aoAjustar(int v, int h) {}
-		public void aoTocar(int telaX, int telaY, int p) {}
-		public void aoSoltar(int telaX, int telaY, int p) {}
-		public void aoArrastar(int telaX, int telaY, int p) {}
-		public void aoFim() {}
 	}
 }
