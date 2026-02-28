d09a5c8112105d0fd7c07ef57a88c2ab47c2f241
Foca-do Estúdios <focadoestudios@gmail.com>
2026-02-28 16:29:42 -0300

    erro de digitação

--- gdx-jogo/src/com/minimine/mundo/Mundo.java
+++ gdx-jogo/src/com/minimine/mundo/Mundo.java
@@ -132,8 +132,8 @@ public class Mundo extends Objeto {
 
 		attChunks((int)jg.posicao.x, (int)jg.posicao.z);
 
-		if(!carregado && etsadps.size() >= 1) {
-			if(estados.get(Chave.calcularChave(0, 0)) == ) carregado = true;
+		if(!carregado && estados.size() >= 1) {
+			if(estados.get(Chave.calcularChave(0, 0)) == 1) carregado = true;
 		}
 		GerenciadorEntidades.att(delta, this, jg);
         if(ciclo) {
